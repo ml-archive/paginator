f50406925740de370515605332c1210396db857a
Brett Toomey <brettcwx@gmail.com>
2017-01-03 15:15:42 -0800

    Finished basic paginator.

--- Sources/Paginator+Entity.swift
+++ Sources/Paginator+Entity.swift
@@ -2,16 +2,33 @@ import HTTP
 import Fluent
 
 extension Entity {
+    /**
+     Constructs a paginator object.
+     
+     - Parameters:
+     - perPage: How many entries per page.
+     - page: Page number to be returned. The initializer will first
+     attempt to decode the page number from the query string,
+     defaulting to this value.
+     - pageName: String to use for the query encoder/decoder. Default is `"page"`.
+     - dataKey: JSON key to store the queried entities in. Default is `"data"`.
+     - request: HTTP Request
+     
+     - Returns: Paginator
+     */
     public static func paginator(
         _ perPage: Int,
-        columns: [String]? = nil,
+        page currentPage: Int = 1,
         pageName: String = "page",
-        page currentPage: Int? = nil,
+        dataKey: String = "data",
         request: Request
     ) throws -> Paginator<Self> {
         return try Paginator(
+            query: Self.query(),
+            currentPage: currentPage,
             perPage: perPage,
             pageName: pageName,
+            dataKey: dataKey,
             request: request
         )
     }
--- Sources/Paginator.swift
+++ Sources/Paginator.swift
@@ -1,3 +1,4 @@
+import URI
 import HTTP
 import Vapor
 import Fluent
@@ -8,13 +9,18 @@ public enum Error: Swift.Error {
 }
 
 public struct Paginator<EntityType: Entity> {
+    public var currentPage: Int
+    public var perPage: Int
+    
     public var total: Int?
-    public var perPage: Int?
-    public var currentPage: Int?
-    public var links: [String]?
+    
+    public var baseURI: URI
+    
+    public var pageName: String
+    public var dataKey: String
     
     public var totalPages: Int? {
-        guard let total = total, let perPage = perPage else {
+        guard let total = total else {
             return nil
         }
         
@@ -27,59 +33,82 @@ public struct Paginator<EntityType: Entity> {
     }
     
     public var previousPage: String? {
-        return nil
+        let previous = currentPage - 1
+        guard previous >= 1 else { return nil }
+        
+        return buildPath(page: previous, count: perPage)
     }
     
     public var nextPage: String? {
-        return nil
+        guard let totalPages = totalPages else { return nil }
+        let next = currentPage + 1
+        guard next <= totalPages else { return nil }
+        
+        return buildPath(page: next, count: perPage)
     }
     
     public var data: Node?
     
-    var query: Query<EntityType>?
+    var query: Query<EntityType>
     
-    init(query: Query<EntityType>, pageName: String? = nil, request: Request) throws {
+    init(
+        query: Query<EntityType>,
+        currentPage: Int = 1,
+        perPage: Int,
+        pageName: String,
+        dataKey: String,
+        request: Request
+    ) throws {
         self.query = query
-        self.data = try extractQueryNode()
-    }
-    
-    init(perPage: Int, pageName: String? = nil, request: Request) throws {
-        if let query = request.query {
-            deserialize(node: query, pageName: pageName)
-        }
-        
+        self.currentPage = currentPage
         self.perPage = perPage
+        self.pageName = pageName
+        self.dataKey = dataKey
         
-        self.query = try EntityType.query()
-        self.data = try extractQueryNode()
+        baseURI = request.uri
+        
+        self.data = try extractEntityData(request.query)
     }
 }
 
 extension Paginator {
-    mutating func deserialize(node: Node, pageName: String?) {
-        currentPage = node[pageName ?? "page"]?.int ?? 0
-        total = node["total"]?.int
-    }
-    
-    mutating func extractQueryNode() throws -> Node {
-        guard let query = query else {
-            throw Error.nilQuery
+    mutating func deserialize(query node: Node) {
+        if let currentPage = node[pageName]?.int {
+            self.currentPage = currentPage
         }
         
-        if let count = perPage {
-            let current = currentPage ?? 0
-            let offset = (current - 1) * count
-            let limit = Limit(count: count, offset: offset)
-            query.limit = limit
+        if let total = node["total"]?.int {
+            self.total = total
         }
+    }
+    
+    mutating func extractEntityData(_ node: Node?) throws -> Node {
+        let page = node?[pageName]?.int ?? currentPage
+        
+        let offset = (page - 1) * perPage
+        let limit = Limit(count: perPage, offset: offset)
+        query.limit = limit
         
         //FIXME: Better caching system
         total = try total ?? EntityType.query().count()
         
         let node = try query.raw()
         
-        
         return node
+    }
+}
+
+extension Paginator {
+    func buildPath(page: Int, count: Int) -> String {
+        return [
+            baseURI.path,
+            "?",
+            pageName,
+            "=",
+            "\(page)",
+            "&count=",
+            "\(count)"
+            ].joined()
     }
 }
 
@@ -99,13 +128,20 @@ extension Paginator: NodeRepresentable, JSONRepresentable, ResponseRepresentable
         }
         
         let node =  try Node(node: [
-            "total": total,
-            "per_page": perPage,
-            "current_page": currentPage,
-            "total_pages": totalPages,
-            "links": links?.makeNode(),
+            "meta": Node(node: [
+                "paginator": Node(node: [
+                    "total": total,
+                    "per_page": perPage,
+                    "current_page": currentPage,
+                    "total_pages": totalPages,
+                    "links": Node(node: [
+                        "previous": previousPage,
+                        "next": nextPage
+                    ]),
+                ])
+            ]),
             
-            "data": data
+            dataKey: data
         ])
         
         return node
