97ddb0706da058b1232fbb52d5c431b608a15f93
Brett Toomey <brettcwx@gmail.com>
2017-01-03 15:17:26 -0800

    Swift comment docs.

--- Sources/Paginator+Entity.swift
+++ Sources/Paginator+Entity.swift
@@ -3,18 +3,18 @@ import Fluent
 
 extension Entity {
     /**
-     Constructs a paginator object.
+        Constructs a paginator object.
      
-     - Parameters:
-     - perPage: How many entries per page.
-     - page: Page number to be returned. The initializer will first
-     attempt to decode the page number from the query string,
-     defaulting to this value.
-     - pageName: String to use for the query encoder/decoder. Default is `"page"`.
-     - dataKey: JSON key to store the queried entities in. Default is `"data"`.
-     - request: HTTP Request
+        - Parameters:
+            - perPage: How many entries per page.
+            - page: Page number to be returned. The initializer will first
+                attempt to decode the page number from the query string,
+                defaulting to this value.
+            - pageName: String to use for the query encoder/decoder. Default is `"page"`.
+            - dataKey: JSON key to store the queried entities in. Default is `"data"`.
+            - request: HTTP Request
      
-     - Returns: Paginator
+        - Returns: Paginator
      */
     public static func paginator(
         _ perPage: Int,
