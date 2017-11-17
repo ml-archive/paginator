52d738bfc2f93b69d560c9a91660198af6d7e584
Mauran Muthiah <mamu@nodes.dk>
2017-11-17 14:50:02 +0100

    Leaf tag now only show first 10 pages

--- Sources/Paginator+Leaf.swift
+++ Sources/Paginator+Leaf.swift
@@ -7,45 +7,45 @@ public final class PaginatorTag: Tag {
         case expectedVariable
         case expectedValidPaginator(message: String)
     }
-    
+
     fileprivate let useBootstrap4: Bool
     fileprivate let paginationLabel: String?
-    
+
     public init(useBootstrap4: Bool = false, paginationLabel: String? = nil) {
         self.useBootstrap4 = useBootstrap4
         self.paginationLabel = paginationLabel
     }
-    
+
     public let name = "paginator"
-    
+
     public func run(
         tagTemplate: TagTemplate,
         arguments: ArgumentList
-    ) throws -> Node? {
+        ) throws -> Node? {
         guard
             arguments.list.count == 1,
             let argument = arguments.list.first
-        else {
+            else {
                 throw Error.expectedOneArgument(got: arguments.count)
         }
-        
+
         guard case .variable(_, let value) = argument else {
             throw Error.expectedVariable
         }
-        
+
         guard let paginator = value?["meta", "paginator"]?.object else {
             throw Error.expectedValidPaginator(message: "Couldn´t find meta: paginator object")
         }
-        
+
         guard
             let currentPage = paginator[Keys.currentPage.key]?.int,
             let totalPages = paginator[Keys.totalPages.key]?.int,
             let links = paginator["links"]?.object
-        else {
-            let keys = paginator.map { $0.key }.joined(separator: ", ")
-            throw Error.expectedValidPaginator(message: "Expected the keys \(Keys.currentPage.key), \(Keys.totalPages.key) and links, got: \(keys)")
+            else {
+                let keys = paginator.map { $0.key }.joined(separator: ", ")
+                throw Error.expectedValidPaginator(message: "Expected the keys \(Keys.currentPage.key), \(Keys.totalPages.key) and links, got: \(keys)")
         }
-        
+
         let queries = paginator["queries"]
 
         return buildNavigation(
@@ -55,20 +55,20 @@ public final class PaginatorTag: Tag {
             queries: queries
         )
     }
-    
+
     public func shouldRender(
         stem: Stem,
         context: Context,
         tagTemplate: TagTemplate,
         arguments: [Argument],
         value: Node?
-    ) -> Bool {
+        ) -> Bool {
         return true
     }
 }
 
 extension PaginatorTag {
-    
+
     func buildBackButton(url: String?) -> Bytes {
         guard let url = url else {
             return buildLink(title: "«", active: false, link: nil, disabled: true).bytes
@@ -76,27 +76,29 @@ extension PaginatorTag {
 
         return buildLink(title: "«", active: false, link: url, disabled: false).bytes
     }
-    
+
     func buildForwardButton(url: String?) -> Bytes {
         guard let url = url else {
             return buildLink(title: "»", active: false, link: nil, disabled: true).bytes
         }
-        
+
         return buildLink(title: "»", active: false, link: url, disabled: false).bytes
     }
-    
+
     func buildLinks(
         currentPage: Int,
         count: Int,
         queries: Node?
-    ) -> Bytes {
+        ) -> Bytes {
         var bytes: Bytes = []
-        
+
         if count == 0 {
             return bytes
         }
-        
-        for i in 1...count {
+
+        let pageSize = count <= 10 ? count : 10
+
+        for i in 1...pageSize {
             let path = PaginatorHelper.buildPath(
                 page: i,
                 count: count,
@@ -109,28 +111,28 @@ extension PaginatorTag {
                     active: true,
                     link: nil,
                     disabled: false
-                ).bytes
+                    ).bytes
             } else {
                 bytes += buildLink(
                     title: "\(i)",
                     active: false,
                     link: path,
                     disabled: false
-                ).bytes
+                    ).bytes
             }
         }
-        
+
         return bytes
     }
-    
+
     func buildNavigation(
         currentPage: Int,
         totalPages: Int,
         links: [String : Node],
         queries: Node?
-    ) -> Node {
+        ) -> Node {
         var bytes: Bytes = []
-        
+
         let navClass: String
         let ulClass: String
         if useBootstrap4 {
@@ -147,25 +149,25 @@ extension PaginatorTag {
         headerString += ">\n<ul class=\"\(ulClass)\">\n"
         let header = headerString.bytes
         let footer = "</ul>\n</nav>".bytes
-        
+
         bytes += header
-        
+
         bytes += buildBackButton(url: links["previous"]?.string)
-        
+
         bytes += buildLinks(currentPage: currentPage, count: totalPages, queries: queries)
-        
+
         bytes += buildForwardButton(url: links["next"]?.string)
-        
+
         bytes += footer
-        
+
         return .bytes(bytes)
     }
-    
+
     func buildLink(title: String, active: Bool, link: String?, disabled: Bool) -> String {
         let linkClass: String?
         let liClass: String?
         let activeSpan = "<span class=\"sr-only\">(current)</span>"
-        
+
         if useBootstrap4 {
             linkClass = "page-link"
             liClass = "page-item"
@@ -173,38 +175,38 @@ extension PaginatorTag {
             linkClass = nil
             liClass = nil
         }
-        
+
         var linkString = "<li"
-        
+
         if active || disabled || liClass != nil {
             linkString += " class=\""
-            
+
             if active {
                 linkString += "active"
             }
             if disabled {
                 linkString += "disabled"
             }
-            
+
             if let liClass = liClass {
                 if active || disabled {
                     linkString += " "
                 }
                 linkString += "\(liClass)"
             }
-            
+
             linkString += "\""
         }
-        
+
         linkString += ">"
-        
+
         if let link = link {
             linkString += "<a href=\"\(link)\""
-            
+
             if let linkClass = linkClass {
                 linkString += " class=\"\(linkClass)\""
             }
-            
+
             if title == "«" {
                 linkString += " rel=\"prev\" aria-label=\"Previous\"><span aria-hidden=\"true\">«</span><span class=\"sr-only\">Previous</span>"
             } else if title == "»" {
@@ -212,29 +214,29 @@ extension PaginatorTag {
             } else {
                 linkString += ">\(title)"
             }
-            
+
         } else {
             linkString += "<a><span"
-            
+
             if let linkClass = linkClass {
                 linkString += " class=\"\(linkClass)\""
             }
-            
+
             if title == "«" {
                 linkString += " aria-label=\"Previous\" aria-hidden=\"true\">«</span><span class=\"sr-only\">Previous</span>"
             } else if title == "»" {
                 linkString += " aria-label=\"Next\" aria-hidden=\"true\">»</span><span class=\"sr-only\">Next</span>"
             } else {
                 linkString += ">\(title)</span>"
-                
+
                 if active {
                     linkString += activeSpan
                 }
             }
         }
-        
+
         linkString += "</a></li>\n"
-        
+
         return linkString
     }
 }
@@ -244,14 +246,15 @@ extension PaginatorTag.Error: Equatable {
         switch (lhs, rhs) {
         case (.expectedOneArgument(let a), .expectedOneArgument(let b)):
             return a == b
-            
+
         case
-            (.expectedVariable, .expectedVariable),
-            (.expectedValidPaginator, .expectedValidPaginator):
-                return true
-            
+        (.expectedVariable, .expectedVariable),
+        (.expectedValidPaginator, .expectedValidPaginator):
+            return true
+
         default:
             return false
         }
     }
 }
+
