389fbbe160b28ec2f6cc6c7080033631d82ca7f0
Martin Lequeux--Gruninger <martin.lequeux.gruninger@gmail.com>
2019-04-13 10:29:02 +0200

    Requested changes for #69

--- Sources/Paginator/Paginatable/RawSQLBuilderPaginatable.swift
+++ Sources/Paginator/Paginatable/RawSQLBuilderPaginatable.swift
@@ -9,10 +9,14 @@ protocol RawSQLBuilderPaginatable: Paginatable {
         source: RawSQLBuilder<D, Result>,
         count: Future<Int>,
         on req: Request
-        ) throws -> Future<([Result], PaginatableMetaData)>
+    ) throws -> Future<([Result], PaginatableMetaData)>
 }
 
-class RawSQLBuilder<Database, Result> where Database: DatabaseKit.Database, Database.Connection: SQLConnectable, Result: Decodable {
+class RawSQLBuilder<Database, Result> where
+    Database: DatabaseKit.Database,
+    Database.Connection: SQLConnectable,
+    Result: Decodable
+{
     let sqlRawBuilder: SQLRawBuilder<Database.Connection>
     let sqlRawCountBuilder: SQLRawBuilder<Database.Connection>?
     
@@ -20,8 +24,14 @@ class RawSQLBuilder<Database, Result> where Database: DatabaseKit.Database, Data
         let count: Int
     }
     
-    init(query: String, countQuery: String, connection: Database.Connection) {
+    init(query: String, countQuery: String?, connection: Database.Connection) {
         self.sqlRawBuilder = connection.raw(query)
+        
+        guard let countQuery = countQuery else {
+            self.sqlRawCountBuilder = nil
+            return
+        }
+        
         self.sqlRawCountBuilder = connection.raw(countQuery)
     }
 }
@@ -39,7 +49,7 @@ extension RawSQLBuilder {
     
     func paginate<P: Paginator>(
         for req: Request
-        ) throws -> Future<P> where
+    ) throws -> Future<P> where
         P: RawSQLBuilderPaginatable,
         P.Object == Result,
         P.PaginatorMetaData == P.PaginatableMetaData
@@ -50,7 +60,7 @@ extension RawSQLBuilder {
     func paginate<P: Paginator>(
         count: Future<Int>,
         for req: Request
-        ) throws -> Future<P> where
+    ) throws -> Future<P> where
         P: RawSQLBuilderPaginatable,
         P.Object == Result,
         P.PaginatorMetaData == P.PaginatableMetaData
@@ -70,7 +80,7 @@ extension RawSQLBuilder: Transformable {
 extension TransformingQuery {
     func paginate<P: Paginator, Database>(
         for req: Request
-        ) throws -> Future<P> where
+    ) throws -> Future<P> where
         P: RawSQLBuilderPaginatable,
         Query: RawSQLBuilder<Database, Result>,
         TransformedResult == P.Object,
@@ -82,17 +92,14 @@ extension TransformingQuery {
     func paginate<P: Paginator, Database>(
         count: Future<Int>,
         for req: Request
-        ) throws -> Future<P> where
+    ) throws -> Future<P> where
         P: RawSQLBuilderPaginatable,
         Query: RawSQLBuilder<Database, Result>,
         TransformedResult == P.Object,
         P.PaginatorMetaData == P.PaginatableMetaData
     {
-        return try P.paginate(
-            source: self.source,
-            count: count,
-            on: req
-            )
+        return try P
+            .paginate(source: self.source, count: count, on: req)
             .flatMap { args -> Future<P> in
                 let (results, data) = args
                 return try self.transform(results).map { results in
--- Sources/Paginator/Paginators/OffsetPaginator/OffsetPaginator+RawSQLBuilderPaginatable.swift
+++ Sources/Paginator/Paginators/OffsetPaginator/OffsetPaginator+RawSQLBuilderPaginatable.swift
@@ -7,7 +7,7 @@ extension OffsetPaginator: RawSQLBuilderPaginatable {
         source: RawSQLBuilder<D, Result>,
         count: Future<Int>,
         on req: Request
-        ) throws -> Future<([Result], OffsetMetaData)> {
+    ) throws -> Future<([Result], OffsetMetaData)> {
         let config: OffsetPaginatorConfig = (try? req.make()) ?? .default
         return try OffsetQueryParams.decode(req: req)
             .flatMap { params in
@@ -15,12 +15,7 @@ extension OffsetPaginator: RawSQLBuilderPaginatable {
                 let perPage = params.perPage ?? config.perPage
                 let lower = (page - 1) * perPage
                 
-                source.sqlRawBuilder.sql.append("""
-                    
-                    LIMIT \(perPage)
-                    OFFSET \(lower)
-                    """
-                )
+                source.sqlRawBuilder.sql.append("\nLIMIT \(perPage)\nOFFSET \(lower)")
                 return count.flatMap { count in
                     let data = try OffsetMetaData(
                         currentPage: page,
