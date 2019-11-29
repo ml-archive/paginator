220a255ec72305ea7c7bccf053dd808ca84a07d9
Rasmus Ebbesen <re@nodes.dk>
2019-11-29 12:11:35 +0100

    added direct tests for new initializer

--- Tests/PaginatorTests/OffsetMetadataTests.swift
+++ Tests/PaginatorTests/OffsetMetadataTests.swift
@@ -45,4 +45,42 @@ class OffsetMetadataTests: XCTestCase {
 
         XCTAssertThrowsError(try OffsetMetadata(currentPage: 0, perPage: 0, total: 10, on: req))
     }
-}
+
+    func testInitNoRequest() throws {
+        let url: URL = URL(string: "https://www.google.com")!
+        let metadata = try OffsetMetadata(currentPage: 0, perPage: 10, total: 200, url: url)
+
+        XCTAssertEqual(metadata.currentPage, 0)
+        XCTAssertEqual(metadata.perPage, 10)
+        XCTAssertEqual(metadata.total, 200)
+        XCTAssertEqual(metadata.totalPages, 20)
+    }
+
+    func testZeroInitNoRequest() throws {
+        let url: URL = URL(string: "https://www.google.com")!
+        let metadata = try OffsetMetadata(currentPage: 0, perPage: 0, total: 0, url: url)
+
+        XCTAssertEqual(metadata.currentPage, 0)
+        XCTAssertEqual(metadata.perPage, 0)
+        XCTAssertEqual(metadata.total, 0)
+        XCTAssertEqual(metadata.totalPages, 0)
+    }
+
+    func testZeroPerPageInitNoRequest() throws {
+        let url: URL = URL(string: "https://www.google.com")!
+        XCTAssertThrowsError(try OffsetMetadata(currentPage: 0, perPage: 0, total: 10, url: url))
+    }
+/*
+    func testNextAndPreviousLinksFirstPage() {
+        let current = 1
+        let total = 10
+        let url: URL = URL(string: "https://www.google.com")!
+
+        let links = OffsetMetadata.nextAndPreviousLinks(
+            currentPage: current,
+            totalPages: total,
+            url: url
+        )
+    }
+*/
+ }
