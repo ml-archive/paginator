0ed263ecf0046fadaabc58381b5d83dcc23bbd31
Rasmus Ebbesen <re@nodes.dk>
2019-12-04 10:26:40 +0100

    Covering all inits for Transformer in tests

--- Tests/PaginatorTests/TransformerTests.swift
+++ Tests/PaginatorTests/TransformerTests.swift
@@ -20,4 +20,66 @@ class TransformerTests: XCTestCase {
 
         XCTAssertEqual(transformedData, expectedOutput)
     }
+
+    func testTransformerInputFutureOutput() throws {
+        let inputData = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        let expectedOutput = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        let eventLoop = EmbeddedEventLoop()
+
+        let transformer = Transformer<Int, String>({ (input: [Int]) -> Future<[String]> in
+            return eventLoop.future(input.map { $0.description })
+        })
+
+        let transformedData: [String] = try transformer
+            .transform(eventLoop.future(inputData))
+            .wait()
+
+        XCTAssertEqual(transformedData, expectedOutput)
+    }
+
+    func testTransformerInputOutputNoArrays() throws {
+        let inputData = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        let expectedOutput = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        let eventLoop = EmbeddedEventLoop()
+
+        let transformer = Transformer<Int, String>({ (input: Int) -> String in
+            return input.description
+        })
+
+        let transformedData: [String] = try transformer
+            .transform(eventLoop.future(inputData))
+            .wait()
+
+        XCTAssertEqual(transformedData, expectedOutput)
+    }
+
+    func testTransformerInputFutureOutputNoArrays() throws {
+        let inputData = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        let expectedOutput = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        let eventLoop = EmbeddedEventLoop()
+
+        let transformer = Transformer<Int, String>({ (input: Int) -> Future<String> in
+            return eventLoop.future(input.description)
+        })
+
+        let transformedData: [String] = try transformer
+            .transform(eventLoop.future(inputData))
+            .wait()
+
+        XCTAssertEqual(transformedData, expectedOutput)
+    }
+
+    func testTransformerInputSameOutputNoArrays() throws {
+        let inputData = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        let eventLoop = EmbeddedEventLoop()
+
+        let transformer = Transformer<Int, Int>()
+
+        let transformedData: [Int] = try transformer
+            .transform(eventLoop.future(inputData))
+            .wait()
+
+        XCTAssertEqual(transformedData, inputData)
+    }
+
 }
